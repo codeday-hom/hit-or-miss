283805fcd97024307aaf02e91a73e159529924a4
Gege Jiang <gegejiang@Geges-MacBook-Pro-16.local>
2023-07-10 13:01:06 -0700

    update readme

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-# HelloWorld
+# Hit or Miss
 
-## Package
-```
-./gradlew distZip
-```
+## Important links:
 
+- [Team board](https://github.com/orgs/codeday-hom/projects/1/views/1)
+- [Slack channel](https://codedayorg.slack.com/archives/C05E9Q49J6N)
+- [This repository](https://github.com/codeday-hom/hit-or-miss)
+
+### How to set up the development environment
+
+< ... >
+
+### How to run all the tests
+
+< ... >
+
+### How to run the game server and play a game
+
+< ... >
--- src/main/kotlin/com/example/HelloWorld.kt
+++ src/main/kotlin/com/example/HelloWorld.kt
@@ -4,15 +4,23 @@ import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
+import org.http4k.core.then
+import org.http4k.filter.DebuggingFilters.PrintRequest
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 
-fun HelloWorld(): HttpHandler {
-    return routes("/" bind GET to { Response(OK).body("hello world!") })
-}
+val app: HttpHandler = routes(
+        "/" bind GET to {
+            Response(OK).body("hello world!")
+        }
+)
 
 fun main() {
-    HelloWorld().asServer(SunHttp(8080)).start()
+    val printingApp: HttpHandler = PrintRequest().then(app)
+
+    val server = printingApp.asServer(SunHttp(8080)).start()
+
+    println("Server started on " + server.port())
 }
