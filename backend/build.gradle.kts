f5cc010922d2e629398ba271a89b3994aada393b
Rob Moore <rmoore@gradle.com>
2023-07-14 10:19:29 +0800

    Format

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -20,7 +20,7 @@ tasks.test {
 dependencies {
     implementation(platform("org.http4k:http4k-bom:5.3.0.0"))
     implementation("org.http4k:http4k-core")
-    implementation ("org.http4k:http4k-server-jetty")
+    implementation("org.http4k:http4k-server-jetty")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.0")
 
     testImplementation(platform("org.junit:junit-bom:5.7.2"))
