28c9ca72f967e760ba52b8ce012d7aa854e736f9
yin <ianchow1271@gmail.com>
2023-07-27 18:11:55 -0700

    added code to give scores to players depending on dice outcome

--- backend/src/test/kotlin/com/game/main/GameTest.kt
+++ backend/src/test/kotlin/com/game/main/GameTest.kt
@@ -28,7 +28,24 @@ class GameTest {
 
     @Test
     fun `play a turn with a miss`() {
-        // The points should be calculated correctly for a miss.
-        fail { "Not implemented" }
+        val ian = Player("ian")
+        val game = Gameplay(ian) // Ian is the host of the game
+        val rob = game.addPlayer("rob")  // Rob and Tom are other players of the game
+        val tom = game.addPlayer("tom")
+        val timmy = game.addPlayer("timmy")
+
+        // Play the first turn of the round
+        // Ian's turn is first and randomly selects a category
+        // Ian rolled the dice and shared a word
+        val ianTurn = game.startTurn(ian, "Ocean Animals", DiceResult.MISS, "Shark")
+        ianTurn.result(rob, TurnResult.MISS) // Rob did not have Shark
+        ianTurn.result(tom, TurnResult.MISS) // Tom did not have Shark
+        ianTurn.result(timmy, TurnResult.HIT)//Timmy did have Shark
+
+
+        assertEquals(2, ian.getPlayerPoints()) // Ian gets a point because Rob had his word
+        assertEquals(3, rob.getPlayerPoints()) // Rob gets a point for having Ian's word
+        assertEquals(3, tom.getPlayerPoints()) // Tom get no points because he didn't have Ian's word
+        assertEquals(0, timmy.getPlayerPoints())
     }
 }
