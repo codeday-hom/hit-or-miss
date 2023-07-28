b59fa089386af727d6dd22af929f3af59d87d446
yin <ianchow1271@gmail.com>
2023-07-27 18:11:55 -0700

    added code to give scores to players depending on dice outcome

--- backend/src/main/kotlin/com/game/main/Gameplay.kt
+++ backend/src/main/kotlin/com/game/main/Gameplay.kt
@@ -15,6 +15,10 @@ class Gameplay(host: Player) {
         players.add(host)
     }
 
+    fun decideTurn(players: MutableList<Player>){
+        //val ianTurn = game.startTurn(ian, "Ocean Animals", DiceResult.MISS, "Shark")
+    }
+
     fun startTurn(selector: Player, category: String, diceResult: DiceResult, selectedWord: String): Turn {
         println("${selector.name} chose $category, rolled $diceResult and selected the word '$selectedWord'")
         return Turn(selector, diceResult)
@@ -33,16 +37,21 @@ class Gameplay(host: Player) {
                         }
 
                         DiceResult.MISS -> {
-                            TODO()
+                            player.addPlayerPoints(0)
+                            selector.addPlayerPoints(0)
                         }
                     }
                 }
 
                 TurnResult.MISS -> {
                     when (diceResult) {
-                        DiceResult.HIT -> {}
+                        DiceResult.HIT -> {
+                            player.addPlayerPoints(0)
+                            selector.addPlayerPoints(0)
+                        }
                         DiceResult.MISS -> {
-                            TODO()
+                            player.addPlayerPoints(3)
+                            selector.addPlayerPoints(1)
                         }
                     }
                 }
