5fd573c357857c9f996d71d57aa0cf269f89b236
yin <ianchow1271@gmail.com>
2023-07-31 16:35:41 -0700

    added turn rotations and more tests

--- backend/src/test/kotlin/com/game/main/GameTest.kt
+++ backend/src/test/kotlin/com/game/main/GameTest.kt
@@ -8,22 +8,36 @@ class GameTest {
 
     @Test
     fun `play a turn with a hit`() {
+
         // Set up a game with Ian as the host, and two other players, Rob and Tom.
         val ian = Player("ian")
         val game = Gameplay(ian) // Ian is the host of the game
         val rob = game.addPlayer("rob")  // Rob and Tom are other players of the game
         val tom = game.addPlayer("tom")
 
+//        Mix who the first selector
+//        game.mixOrder()
+
         // Play the first turn of the round
         // Ian's turn is first and randomly selects a category
         // Ian rolled the dice and shared a word
-        val ianTurn = game.startTurn(ian, "Ocean Animals", DiceResult.HIT, "Shark")
-        ianTurn.result(rob, TurnResult.HIT) // Rob had Shark
-        ianTurn.result(tom, TurnResult.MISS) // Tom didn't have Shark
+        val firstTurn = game.startTurn(game.nextTurn(), "Ocean Animals", DiceResult.HIT, "Shark")
+        firstTurn.result(rob, TurnResult.HIT) // Rob had Shark
+        firstTurn.result(tom, TurnResult.MISS) // Tom didn't have Shark
 
         assertEquals(1, ian.getPlayerPoints()) // Ian gets a point because Rob had his word
         assertEquals(1, rob.getPlayerPoints()) // Rob gets a point for having Ian's word
         assertEquals(0, tom.getPlayerPoints()) // Tom get no points because he didn't have Ian's word
+
+
+        val secondTurn = game.startTurn(game.nextTurn(), "Fruits", DiceResult.HIT, "Apple")
+        secondTurn.result(tom, TurnResult.HIT) // Tom had Apple
+        secondTurn.result(ian, TurnResult.MISS) // ian didn't have Apple
+
+        assertEquals(1, ian.getPlayerPoints())
+        assertEquals(2, rob.getPlayerPoints())
+        assertEquals(1, tom.getPlayerPoints())
+
     }
 
     @Test
@@ -37,15 +51,26 @@ class GameTest {
         // Play the first turn of the round
         // Ian's turn is first and randomly selects a category
         // Ian rolled the dice and shared a word
-        val ianTurn = game.startTurn(ian, "Ocean Animals", DiceResult.MISS, "Shark")
+        val ianTurn = game.startTurn(game.nextTurn(), "Ocean Animals", DiceResult.MISS, "Shark")
         ianTurn.result(rob, TurnResult.MISS) // Rob did not have Shark
         ianTurn.result(tom, TurnResult.MISS) // Tom did not have Shark
         ianTurn.result(timmy, TurnResult.HIT)//Timmy did have Shark
 
 
         assertEquals(2, ian.getPlayerPoints()) // Ian gets a point because Rob had his word
-        assertEquals(3, rob.getPlayerPoints()) // Rob gets a point for having Ian's word
-        assertEquals(3, tom.getPlayerPoints()) // Tom get no points because he didn't have Ian's word
-        assertEquals(0, timmy.getPlayerPoints())
+        assertEquals(0, rob.getPlayerPoints()) // Rob gets a point for having Ian's word
+        assertEquals(0, tom.getPlayerPoints()) // Tom get no points because he didn't have Ian's word
+        assertEquals(3, timmy.getPlayerPoints())
+
+        val Turn = game.startTurn(game.nextTurn(), "Veggies", DiceResult.MISS, "Carrot")
+        Turn.result(tom, TurnResult.HIT)
+        Turn.result(timmy, TurnResult.MISS)
+        Turn.result(ian, TurnResult.HIT)
+
+
+        assertEquals(5, ian.getPlayerPoints())
+        assertEquals(1, rob.getPlayerPoints())
+        assertEquals(3, tom.getPlayerPoints())
+        assertEquals(3, timmy.getPlayerPoints())
     }
 }
