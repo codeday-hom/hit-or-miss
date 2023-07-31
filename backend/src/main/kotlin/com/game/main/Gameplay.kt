fabbf1710491eefd875f8268053d695834b513ea
yin <ianchow1271@gmail.com>
2023-07-31 16:35:41 -0700

    added turn rotations and more tests

--- backend/src/main/kotlin/com/game/main/Gameplay.kt
+++ backend/src/main/kotlin/com/game/main/Gameplay.kt
@@ -1,4 +1,6 @@
 package com.game.main
+import java.util.Random
+
 
 enum class DiceResult {
     HIT, MISS
@@ -10,19 +12,33 @@ enum class TurnResult {
 
 class Gameplay(host: Player) {
     private val players: MutableList<Player> = mutableListOf()
+    private val random = Random()
+    private var currentPlayerIndex = 0
+
+    fun getPlayer(index: Int): Player{
+        return players[index]
+    }
 
     init {
         players.add(host)
     }
 
-    fun decideTurn(players: MutableList<Player>){
-        //val ianTurn = game.startTurn(ian, "Ocean Animals", DiceResult.MISS, "Shark")
+    fun mixOrder(){
+        players.shuffle(random)
+    }
+
+    fun nextTurn(): Player{
+        val firstPlayer = players[currentPlayerIndex]
+        currentPlayerIndex = (currentPlayerIndex + 1) % players.size
+        return firstPlayer
+
     }
 
     fun startTurn(selector: Player, category: String, diceResult: DiceResult, selectedWord: String): Turn {
         println("${selector.name} chose $category, rolled $diceResult and selected the word '$selectedWord'")
         return Turn(selector, diceResult)
     }
+
 
     class Turn(private val selector: Player, private val diceResult: DiceResult) {
 
@@ -37,7 +53,7 @@ class Gameplay(host: Player) {
                         }
 
                         DiceResult.MISS -> {
-                            player.addPlayerPoints(0)
+                            player.addPlayerPoints(3)
                             selector.addPlayerPoints(0)
                         }
                     }
@@ -50,7 +66,7 @@ class Gameplay(host: Player) {
                             selector.addPlayerPoints(0)
                         }
                         DiceResult.MISS -> {
-                            player.addPlayerPoints(3)
+                            player.addPlayerPoints(0)
                             selector.addPlayerPoints(1)
                         }
                     }
