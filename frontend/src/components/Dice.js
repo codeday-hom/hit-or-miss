938863b8e56a58a44b2951f8b863952fb8cfab0b
zhoumingwu <wu.zhoum@northeastern.edu>
2023-08-23 19:32:54 +0800

    Add the wildcard possibility

--- frontend/src/components/Dice.js
+++ frontend/src/components/Dice.js
@@ -3,11 +3,20 @@ import "./Dice.css";
 
 function Dice() {
   const [diceState, setDiceState] = useState("");
+  const [wildcardOption, setWildcardOption] = useState(false);
 
   function rollDice() {
     const diceResult = Math.floor(Math.random() * 6) + 1;
     console.log(diceResult);
     setDiceState("show-" + diceResult);
+    if (diceResult === 6) {
+      setWildcardOption(true);
+    }
+  }
+
+  function handleWildcardOption(option) {
+    console.log("Player chose", option);
+    setWildcardOption(false);
   }
 
   return (
@@ -30,13 +39,24 @@ function Dice() {
             <div className="miss-text">Miss</div>
           </div>
           <div className="side">
-            <div className="miss-text">Miss</div>
+            <div className="wildcard-text">Wildcard</div>
           </div>
         </div>
       </div>
       <div className="roll-button">
         <button onClick={rollDice}>Roll dice</button>
       </div>
+
+      {wildcardOption && (
+        <div className="wildcard">
+          <div className="wildcard-content">
+            <h2>Wildcard!</h2>
+            <p>Hit or Miss?</p>
+            <button onClick={() => handleWildcardOption("Hit")}>Hit</button>
+            <button onClick={() => handleWildcardOption("Miss")}>Miss</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
