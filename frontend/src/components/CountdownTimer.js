1d7f39eab62901f32b65d579bc00953ec22e7556
ichow3 <ichow@cpp.edu>
2023-08-27 20:17:50 -0700

    added countdown timer component

--- frontend/src/components/CountdownTimer.js
+++ frontend/src/components/CountdownTimer.js
@@ -48,7 +48,7 @@ function CountdownTimer() {
             case 'go':
                 return 'Go!';
             case 'timeout':
-                return 'Time\'s up!';
+                return "Time's up!";
             default:
                 return '';
         }
