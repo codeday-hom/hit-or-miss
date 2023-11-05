cea869fc4d0134024b7e32df616407576e250686
Rob Moore <rmoore@gradle.com>
2023-11-05 10:12:41 +0800

    Make the docker build task cacheable

--- deployment/build.gradle.kts
+++ deployment/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.io.IOException
+
 plugins {
     base
     id("com.palantir.git-version") version "3.0.0"
@@ -19,7 +21,7 @@ dependencies {
 }
 
 tasks {
-    val dockerDir by registering(Copy::class) {
+    val dockerDirBuild by registering(Copy::class) {
         from("src/docker")
         from(backendShadowJar)
         into(layout.buildDirectory.dir(name))
@@ -34,15 +36,23 @@ tasks {
     }
 
     val gitVersion: groovy.lang.Closure<String> by extra
-    val dockerImageName = "hit-or-miss:${gitVersion().replace(".", "-")}"
-    val dockerBuild by registering(DockerTask::class) {
-        workingDir.set(dockerDir.map { it.destinationDir })
-        args.set(listOf("build", "-t", dockerImageName, "."))
+    val dockerBuildAndSave by registering(DockerBuildAndSaveTask::class) {
+        val dockerImageName = "hit-or-miss:${gitVersion().replace(".", "-")}"
+        buildDir.set(dockerDirBuild.map { it.destinationDir })
+        imageName.set(dockerImageName)
+        outputTar.set(layout.buildDirectory.file("$name/${dockerImageName.replace(":", "--")}.tar"))
+    }
+
+    val dockerImport by registering(DockerTask::class) {
+        args.add("import")
+        args.add(dockerBuildAndSave.flatMap { task -> task.outputTar.map { it.asFile.absolutePath } })
+        args.add(dockerBuildAndSave.flatMap { it.imageName })
     }
 
     val dockerRun by registering(DockerTask::class) {
-        dependsOn(dockerBuild)
-        args.set(listOf("run", "-p", "8080:8080", dockerImageName))
+        dependsOn(dockerBuildAndSave)
+        args.set(listOf("run", "-p", "8080:8080", "--name", "hit-or-miss"))
+        args.add(dockerBuildAndSave.flatMap { it.imageName })
     }
 }
 
@@ -58,12 +68,45 @@ abstract class DockerTask @Inject constructor(private val execOps: ExecOperation
     @TaskAction
     fun runDockerCommand() {
         val dockerCommandArgs = listOf("docker") + args.get()
+        logger.info("Running '${dockerCommandArgs.joinToString(" ")}'.")
         val dockerWorkingDir = workingDir
         execOps.exec {
             if (dockerWorkingDir.isPresent) {
                 workingDir(dockerWorkingDir.get())
             }
             commandLine(dockerCommandArgs)
+        }
+    }
+}
+
+@CacheableTask
+abstract class DockerBuildAndSaveTask @Inject constructor(private val execOps: ExecOperations) : DefaultTask() {
+
+    @get:Input
+    abstract val buildDir: Property<File>
+
+    @get:Input
+    abstract val imageName: Property<String>
+
+    @get:OutputFile
+    abstract val outputTar: RegularFileProperty
+
+    @TaskAction
+    fun runDockerCommands() {
+        // Build
+        val dockerBuildCommand = listOf("docker", "build", "-t", imageName.get(), ".")
+        execOps.exec {
+            workingDir(buildDir.get())
+            commandLine(dockerBuildCommand)
+        }
+        // Save
+        val outputTarFile = outputTar.get().asFile
+        if (!outputTarFile.parentFile.exists() && !outputTarFile.parentFile.mkdirs()) {
+            throw IOException("Couldn't create output directory: '${outputTarFile.parentFile.absolutePath}'.")
+        }
+        val dockerSaveCommand = listOf("docker", "save", imageName.get(), "--output", outputTarFile.absolutePath)
+        execOps.exec {
+            commandLine(dockerSaveCommand)
         }
     }
 }
