d3906bf47139aa0a4197b3b806e0a9892cf9c796
Rob Moore <rmoore@gradle.com>
2023-12-24 20:42:38 +0000

    Copy changes and horizontal alignment

--- frontend/src/Landing.js
+++ frontend/src/Landing.js
@@ -4,11 +4,11 @@ export default function Landing() {
   let version = process.env.REACT_APP_GIT_VERSION
   let versionLinkPart = version.replace("-snapshot", "")
   return (
-    <div>
+    <div className="buttonbox">
       <form action="/api/new-game" method="post">
-        <button type="submit">Create new game</button>
+        <button type="submit">Play online with friends</button>
       </form>
-      <a href="/kit"><button>Categories and Dice</button></a>
+      <a href="/kit"><button>Dice and categories for playing in person</button></a>
       <br/>
       <a href={`https://github.com/codeday-hom/hit-or-miss/tree/${versionLinkPart}`}>Version {version}</a>
     </div>
